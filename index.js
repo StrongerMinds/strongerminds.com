009aefaff90bc4177045219c4582fb1b350e6bb2
StrongerMinds <strongermindstoronto@gmail.com>
2025-05-13 13:15:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import StrongerMindsHome from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StrongerMindsHome />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
